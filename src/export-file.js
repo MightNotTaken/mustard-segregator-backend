1a405290d7eca87a8750d3213592d31d07aa3234
Tahir <mightnottaken@gmail.com>
2023-01-04 22:07:04 +0530

    removed index.json file from export paths

--- src/export-file.js
+++ src/export-file.js
@@ -48,6 +48,9 @@ class ExportComponent {
     export = function(id) {
         return new Promise(async (res, rej) => {
             try {
+                if (this.path[id] === 'index.json') {
+                    return res(true);
+                }
                 const sourcePath = path.join(__dirname, './../database', this.paths[id]);
                 const destinationPath = path.join(await this.getDestinationPath(), this.paths[id]);
                 fs.copyFileSync(sourcePath, destinationPath);
